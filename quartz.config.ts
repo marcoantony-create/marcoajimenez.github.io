fed0cc5a3ff093a60625ccb533ad639da117151a
Marco Jimenez <marcoatony@gmail.com>
2025-12-20 15:51:20 +0100

    Fix contentDirectory syntax and path

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,6 +7,8 @@ import * as Plugin from "./quartz/plugins"
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
 const config: QuartzConfig = {
+  contentDirectory: "G:/My Drive/2025/Obsidian-Site/obsidian-site",
+
   configuration: {
     pageTitle: "Quartz 4",
     pageTitleSuffix: "",
