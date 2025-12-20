8272524fce498a897728c58f6dabb59e684d7d52
Marco Jimenez <marcoatony@gmail.com>
2025-12-20 15:33:19 +0100

    Set baseUrl for GitHub Pages project site

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "marcoantony-create.github.io/marcoajimenez.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
